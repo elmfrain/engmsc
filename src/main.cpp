872c4eff2ca4b58ad69f50fcc4beaba8f91b6772
elmfrain <elmfer10@gmail.com>
2022-10-15 02:26:50 -0500

    Main: handle multiple gauges

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,9 @@ int main(int argc, char* argv[])
 
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_STENCIL_BITS, 8);
+    glfwWindowHint(GLFW_RED_BITS, 16);
+    glfwWindowHint(GLFW_GREEN_BITS, 16);
+    glfwWindowHint(GLFW_BLUE_BITS, 16);
 
     EMWindow window(1280, 720, "Engmsc by Elmfer");
     const EMKeyboard& keyboard = window.getKeyboard();
@@ -47,39 +50,36 @@ int main(int argc, char* argv[])
     emui::setWindow(window);
 
     // Create gauges
-    EMGauge tach;
-    tach.setText("RPMx1000");
-    EMGauge::Profile& tachProfile = tach.getProfile();
-    tachProfile.radius = 120;
-    tachProfile.numMarkings = 9;
-    tachProfile.subdivisions = 3;
-    tach.applyProfile();
-    tach.setRange(0, 8);
-    EMGauge baro;
-    baro.setText("Intake kPa");
-    EMGauge::Profile& baroProfile = baro.getProfile();
-    baroProfile.radius = 120;
-    baroProfile.numMarkings = 9;
-    baroProfile.subdivisions = 3;
-    baro.applyProfile();
-    baro.setRange(0, 200);
-
-    EMGauge baro2;
-    baro2.setText("Exhaust kPa");
-    baro2.getProfile() = baroProfile;
-    baro2.applyProfile();
-    baro2.setRange(0, 200);
+    typedef std::shared_ptr<EMGauge> Gauge;
+    std::vector<Gauge> gauges;
+    gauges.push_back(std::make_shared<EMGauge>()); // RPM Gauge
+    gauges.back()->setText("RPM x1000");
+    gauges.back()->setRange(0, 8);
+    gauges.back()->getProfile().subdivisions = 3;
+    gauges.push_back(std::make_shared<EMGauge>()); // Cyl Baro Gauge
+    gauges.back()->setText("Cyl kPa");
+    gauges.back()->setRange(0, 800);
+    gauges.back()->getProfile().subdivisions = 3;
+    gauges.push_back(std::make_shared<EMGauge>()); // Cyl Gas Mass Gauge
+    gauges.back()->setText("Cyl Gas kg");
+    gauges.back()->setRange(0, 2);
+    gauges.back()->getProfile().precision = 1;
+    gauges.back()->getProfile().subdivisions = 3;
+    gauges.push_back(std::make_shared<EMGauge>()); // Intake Baro Gauge
+    gauges.back()->setText("Exhaust kPa");
+    gauges.back()->setRange(0, 400);
+    gauges.back()->getProfile().subdivisions = 3;
+    for(Gauge& gauge : gauges)
+    {
+        gauge->getProfile().radius = 120;
+    }
 
     // Create Engine Assembly
     EMEngineAssembly engine;
     engine.frictionTorque = 5000;
     engine.rotationalMass = 1000;
     engine.cylinders.back().deckClearance = 0.1f;
-    engine.cylinders.back().bankAngle = -0.785;
-    engine.cylinders.emplace_back();
-    engine.cylinders.back().deckClearance = 0.1f;
-    engine.cylinders.back().angleOffset = 4.712f;
-    engine.cylinders.back().bankAngle = 0.785;
+    //engine.cylinders.back().angleOffset = glm::half_pi<double>();
 
     // Init engine renderer and physics
     EMEngine2DRenderer::init();
@@ -95,7 +95,7 @@ int main(int argc, char* argv[])
     audioCtx.addStream(audStream);
     audStream.play(EMAudioEvent(EMEngineAudio::getAudioProducer()));
 
-    EMTimer timer(10000.0);
+    EMTimer timer(6000.0);
 
     while(!window.shouldClose())
     {
@@ -107,33 +107,39 @@ int main(int argc, char* argv[])
 
         // Render engine
         double torque = 0.0;
-        if(keyboard.isKeyPressed(GLFW_KEY_LEFT)) torque = -300000;
-        if(keyboard.isKeyPressed(GLFW_KEY_RIGHT)) torque = 300000;
+        if(keyboard.isKeyPressed(GLFW_KEY_LEFT)) torque = -50000;
+        if(keyboard.isKeyPressed(GLFW_KEY_RIGHT)) torque = 50000;
         int steps = timer.ticksPassed();
         for(int i = 0; i < steps; i++)
         {
             EMEnginePhysics::applyTorque(torque);
             EMEnginePhysics::update(1.0 / timer.getTPS(), 1);
+            //EMEnginePhysics::update(0.009 / timer.getTPS(), 1);
         }
         EMEngine2DRenderer::render();
 
         // UI Rendering
         emui::setupUIRendering();
 
+        // Render gauges
         float rpm = 60.0f * float(engine.crankSpeed / glm::two_pi<double>());
-        tach.x = window.getWidth() - 120.0f;
-        tach.y = baro.y = baro2.y = window.getHeight() - 120.0f;
-        tach.setValue(rpm / 1000.0f);
-        tach.draw();
-        baro.x = tach.x - 240;
-        baro.setValue((float) EMEnginePhysics::getIntakePressure(0) * 1e-3f);
-        baro.draw();
-        baro2.x = baro.x - 240;
-        baro2.setValue((float) EMEnginePhysics::getExhaustPressure(0) * 1e-3f);
-        baro2.draw();
+        gauges[0]->setValue(rpm / 1000);
+        gauges[1]->setValue((float) EMEnginePhysics::getCylPressure(0) * 1e-3f);
+        gauges[2]->setValue((float) EMEnginePhysics::getCylGasMass(0));
+        gauges[3]->setValue((float) EMEnginePhysics::getIntakePressure(0) * 1e-3f);
+        float gaugex = emui::getUIWidth();
+        for(Gauge& gauge : gauges)
+        {
+            float radius = gauge->getProfile().radius;
+            gaugex -= radius;
+            gauge->x = gaugex;
+            gauge->y = emui::getUIHeight() - radius;
+            gauge->draw();
+            gaugex -= radius;
+        }
 
-        emui::genString(std::to_string(audStream.getNbEvents()).c_str(), 0, 0, 0xFFFFFFFF, emui::TOP_LEFT);
-        emui::genString(std::to_string(EMEngineAudio::m_engineLog.size()).c_str(), 100, 0, 0xFFFFFFFF, emui::TOP_LEFT);
+        emui::genString(std::to_string(EMEnginePhysics::getCylPressure(0)).c_str(), 0, 0, 0xFFFFFFFF, emui::TOP_LEFT);
+        emui::genString(std::to_string(EMEngineAudio::m_engineLog.size()).c_str(), 300, 0, 0xFFFFFFFF, emui::TOP_LEFT);
 
         emui::renderBatch();
 
